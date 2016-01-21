530484f21345d156953e0fac7cb0a996e07478e8
vik denic <vik.denic@gmail.com>
2016-01-21 12:29:37 -0600

    Initial Commit

---
+++ DragMeDownTests/DragMeDownTests.swift
@@ -0,0 +1,36 @@
+//
+//  DragMeDownTests.swift
+//  DragMeDownTests
+//
+//  Created by Vik Denic on 1/21/16.
+//  Copyright © 2016 nektar labs. All rights reserved.
+//
+
+import XCTest
+@testable import DragMeDown
+
+class DragMeDownTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
